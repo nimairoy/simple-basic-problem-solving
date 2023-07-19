dc37de21b58f92caf778c47dd425ee8b77900742
nimairoy <nimairoy718@gmail.com>
2023-07-19 14:24:29 +0600

    problem 3 solve

---
+++ problem3.js
@@ -0,0 +1,19 @@
+// You are given three numbers 13, 79, and 45. Write a program that will print the
+// largest number using if-else.
+
+const largestNumber = (x, y, z) => {
+    if(x > y && x > z){
+        console.log('The Largest Number is', x);
+    }
+    else if(y > x && y > z){
+        console.log('The Largest Number is', y);
+    }
+    else if(z > x && z > y){
+        console.log('The Largest Number is', z);
+    }
+    else{
+        console.log('Invalid Number')
+    }
+}
+
+largestNumber(13, 79, 45);
